beb04cbd9b879defca58eaafc3743126a390b41f
Ranol <asdhugh1@gmail.com>
2017-11-22 21:40:15 +0900

    Fix error in test

--- src/test/kotlin/io/github/ranolp/latte/compiler/ComplierTest.kt
+++ src/test/kotlin/io/github/ranolp/latte/compiler/ComplierTest.kt
@@ -3,10 +3,12 @@ package io.github.ranolp.latte.compiler
 import io.github.ranolp.latte.compiler.frontend.Lexer
 import org.junit.Test
 
-@Test
-fun `test compiler`() {
-    val tokens = Lexer.lex("package latte.test\nfn main {\nprintln(\"Hello, Latte!\")\n}")
-    for (token in tokens) {
-        println(token)
+class ComplierTest {
+    @Test
+    fun `test compiler`() {
+        val tokens = Lexer.lex("package latte.test\nfn main {\nprintln(\"Hello, Latte!\")\n}")
+        for (token in tokens) {
+            println(token)
+        }
     }
 }
