3a6d17eec703ada773642fc23bc9494680e099a8
Ranol <asdhugh1@gmail.com>
2017-11-28 18:54:10 +0900

    Update LiteralNode

--- src/main/kotlin/io/github/ranolp/latte/compiler/backend/core/ast/LiteralNode.kt
+++ src/main/kotlin/io/github/ranolp/latte/compiler/backend/core/ast/LiteralNode.kt
@@ -2,6 +2,14 @@ package io.github.ranolp.latte.compiler.backend.core.ast
 
 import io.github.ranolp.latte.compiler.core.Token
 
-class IntNode(token: Token) : Node("Int", token, 1)
-class DecimalNode(token: Token) : Node("Decimal", token, 1)
-class StringNode(token: Token) : Node("String", token, 1)
+class IntNode(token: Token) : Node("Int", token, 1) {
+    val value: Int = token.data.toInt()
+}
+
+class DecimalNode(token: Token) : Node("Decimal", token, 1) {
+    val value: Double = token.data.toDouble()
+}
+
+class StringNode(token: Token) : Node("String", token, 1) {
+    val value: String = token.data
+}
