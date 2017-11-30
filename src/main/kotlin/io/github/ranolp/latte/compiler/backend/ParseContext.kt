16909b0e15b1f2bd0d8cf992c82ef6da5c083166
Ranol <asdhugh1@gmail.com>
2017-11-30 22:53:37 +0900

    Remove unused file

--- src/main/kotlin/io/github/ranolp/latte/compiler/backend/ParseContext.kt
+++
@@ -1,9 +0,0 @@
-package io.github.ranolp.latte.compiler.backend
-
-import io.github.ranolp.latte.compiler.core.Token
-
-data class ParseContext(val tokens: List<Token>) {
-    fun require(run: ParseContext.() -> Unit) {
-
-    }
-}
