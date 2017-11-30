43deb58d47f6fd33cff7de50b21c588365069d46
Ranol <asdhugh1@gmail.com>
2017-11-30 20:24:51 +0900

    Parsed result

--- src/test/kotlin/io/github/ranolp/latte/compiler/CompilerTest.kt
+++ src/test/kotlin/io/github/ranolp/latte/compiler/CompilerTest.kt
@@ -1,5 +1,6 @@
 package io.github.ranolp.latte.compiler
 
+import io.github.ranolp.latte.compiler.backend.Parser
 import io.github.ranolp.latte.compiler.frontend.Lexer
 import org.junit.Test
 
@@ -9,6 +10,12 @@ class CompilerTest {
         val tokens = Lexer.lex("package latte.test\nfn main {\nprintln(\"Hello, Latte!\")\n}")
         for (token in tokens) {
             println(token)
+        }
+        val parsed = Parser.parse(tokens)
+        if(parsed !== null) {
+            println("Parse Success : ${parsed.debug()}")
+        } else {
+            println("Parse Failure : $parsed")
         }
     }
 }
